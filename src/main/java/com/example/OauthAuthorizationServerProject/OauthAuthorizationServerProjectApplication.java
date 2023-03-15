e70879ed249ed2e413eb1dc6d2ea27eb4cf77e80
vishesh-baghel <visheshbaghel99@gmail.com>
2023-03-15 12:16:30 +0530

    Initial commit

---
+++ src/main/java/com/example/OauthAuthorizationServerProject/OauthAuthorizationServerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.OauthAuthorizationServerProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthAuthorizationServerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthAuthorizationServerProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/OauthAuthorizationServerProject/OauthAuthorizationServerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OauthAuthorizationServerProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthAuthorizationServerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
